12fe7bfb92558127328d6a1b2b0ce6a172d76071
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-03-25 04:23:45 +0200

    Add bonus

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,13 @@
+-- creates a stored procedure AddBonus that adds a new correction for a student.
+DELIMITER //
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+    IF (SELECT id FROM projects WHERE name = project_name) IS NULL THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SET @project_id = LAST_INSERT_ID();
+    ELSE
+        SELECT id INTO @project_id FROM projects WHERE name = project_name;
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, @project_id, score);
+END //
+DELIMITER ;
