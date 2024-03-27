dd6f03957c25ce6754dccd5876edcf7782b61e07
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2024-03-27 20:54:43 +0200

    Log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" 12-log_stats """
+
+from pymongo import MongoClient
+
+
+def log_stats(mongo_collection) -> None:
+    """
+    Log stats from a MongoDB collection.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection):
+        The MongoDB collection to log stats from.
+    """
+    total_logs = mongo_collection.count_documents({})
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_count = {method: mongo_collection.count_documents(
+        {"method": method}) for method in methods}
+
+    status_count = mongo_collection.count_documents({"method": "GET",
+                                                     "path": "/status"})
+
+    return total_logs, method_count, status_count
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+
+    total_logs, method_count, status_count = log_stats(collection)
+
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method, count in method_count.items():
+        print(f"\tmethod {method}: {count}")
+    print(f"{status_count} status check")
